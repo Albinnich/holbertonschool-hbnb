d267480405e17dbdc8a857298284eb3579f1bee0
Albinnich <albina_haji@hotmail.com>
2024-06-15 17:42:36 +0000

    Business logic added

--- hbnb/models/place.py
+++ hbnb/models/place.py
@@ -2,21 +2,24 @@ import uuid
 from datetime import datetime
 
 class Place:
-    def __init__(self, name, description, number_rooms, max_guest, price_by_night):
+    def __init__(self, name, description, number_rooms, max_guest, price_by_night, user_id, city_id, amenity_ids=None):
         self.id = str(uuid.uuid4())
         self.name = name
         self.description = description
         self.number_rooms = number_rooms
         self.max_guest = max_guest
         self.price_by_night = price_by_night
+        self.user_id = user_id
+        self.city_id = city_id
+        self.amenity_ids = amenity_ids or []
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
-
-    def __str__(self):
-        return f"[Place] ({self.id}) {self.__dict__}"
 
     def save(self):
         self.updated_at = datetime.now()
 
     def delete(self)
         pass
+
+    def __str__(self):
+        return f"[Place] ({self.id}) {self.__dict__}"
--- hbnb/models/user.py
+++ hbnb/models/user.py
@@ -2,7 +2,11 @@ import uuid
 from datetime import datetime
 
 class User:
+    emails = set()
+
     def __init__(self, email, first_name, last_name, password):
+        if email in User.emails:
+            raise ValueError("Email is not available")
         self.id = str(uuid.uuid4())
         self.email = email
         self.first_name = first_name
@@ -10,12 +14,13 @@ class User:
         self.password = password
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
-
-    def __str__(self):
-        return f"[User] ({self.id}) {self.__dict__}"
+        User.emails.add(email)
 
     def save(self):
         self.updated_at = datetime.now()
 
     def delete(self):
-        pass
+        User.emails.remove(self.email)
+
+    def __str__(self):
+        return f"[User] ({self.id}) {self.__dict__)"
