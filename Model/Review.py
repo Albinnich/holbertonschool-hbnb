3d033debc9418d596d12da543ce601968581b2bf
Az1z67 <abbasovaziz61@gmail.com>
2024-08-01 12:04:50 +0400

    commit

---
+++ Model/Review.py
@@ -0,0 +1,12 @@
+from Model.basemodel import BaseModel
+
+class Review(BaseModel):
+    def __init__(self, place_id, user_id, rating, comment):
+        super().__init__()
+        self.place_id = place_id
+        self.user_id = user_id
+        self.rating = rating
+        self.comment = comment
+        
+        def __str__(self):
+            return self.id
---
+++ Model/User.py
@@ -0,0 +1,12 @@
+from Model.basemodel import BaseModel
+
+class User(BaseModel):
+    def __init__(self, email, first_name, last_name, password=""):
+        super().__init__()
+        self.email = email
+        self.password = password
+        self.first_name = first_name
+        self.last_name = last_name
+        
+        def __str__(self):
+            return self.id
